97e8bf1ceb466ac68157008262ea37538f9cee50
Alex Henrique <alexsctj@gmail.com>
2022-08-04 19:32:21 -0300

    update

--- script-iac2.sh
+++ script-iac2.sh
@@ -5,9 +5,9 @@ apt-get upgrade - y
 
 echo "Instalando programas..."
 apt-get install apache2 -y
-apt-get install unzip - y
-apt-get install wget - y
-apt-get install git - y
+apt-get install unzip -y
+apt-get install wget -y
+apt-get install git -y
 
 echo "Criando pasta tmp..."
 mkdir tmp
