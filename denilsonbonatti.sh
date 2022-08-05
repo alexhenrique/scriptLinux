cf7cdedac9c1ccb18baf7e998412aae3808a25f8
Alex Henrique <alexsctj@gmail.com>
2022-08-05 08:51:15 -0300

    final

--- denilsonbonatti.sh
+++ denilsonbonatti.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
-echo " !!! Criando Diretorios !!!"
+
+echo "Criando diretórios..."
+
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
-echo " !!! Diretorios Criado !!!"
 
-echo " !!! Criando Grupos !!!"
+echo "Criando grupos de usuários..."
+
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
-echo " !!! Tarefa Criando Grupos finalizada !!!"
 
-echo " !!! Criando usuarios do sistema !!!"
+echo "Criando usuários..."
 
-echo " !!! Criando usuarios ADM !!!"
 useradd carlos -c /bin/bash -c $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-echo " !!! Tarefa Criando usuarios ADM finalizada !!!"
 
-
-echo " !!! Criando usuarios vendas !!!"
 useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-
-echo " !!! Criando usuarios Secretariado !!!"
 useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-echo "!!! Tarefa criando usuarios secretariado finalizada !!!"
 
-echo "!!! Especificando permissões dos diretórios !!!"
+echo "Especificando permissões dos diretórios...."
+
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
@@ -43,4 +38,4 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo " !!! Tarefa finalizada com sucesso !!!"
+echo "Fim....."
